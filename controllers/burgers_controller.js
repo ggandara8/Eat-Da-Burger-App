b97eafc2109b02470cd04a6bedd23269b59dad7b
ggandara8 <ggandara8@gmail.com>
2020-06-29 14:20:03 -0500

    changed on controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,13 +3,13 @@ var router = express.Router();
 var burger = require("../models/burger");
 
 router.get("/", function(req, res){
-    burger.all(function(data){
-      var hbsObject = {
-          burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
+  burger.all(function(data){
+    var hbsObject = {
+        burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 });
 
 router.post("/api/burgers", function(req, res){
@@ -27,7 +27,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  cat.update({
+  burger.update({
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
