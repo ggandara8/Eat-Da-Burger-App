e579c4d7c2b7a35c98ae6dab0e89a4c9aa1947ee
ggandara8 <ggandara8@gmail.com>
2020-06-29 21:31:52 -0500

    handlebars and css working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,7 +15,7 @@ router.get("/", function(req, res){
 router.post("/api/burgers", function(req, res){
   burger.create([
     "burger_name", "devoured"
-  ], [
+  ],[
     req.body.burger, req.body.devoured
   ], function(result){
     res.json({ id: result.insertId });
